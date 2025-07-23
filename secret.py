9d194341810b9b671250633536aeae061775e6df
kishanHashicorp <kishan.kumar@hashicorp.com>
2025-07-23 22:10:15 +0530

    Update secret.py

--- secret.py
+++ secret.py
@@ -2,7 +2,7 @@
 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
 
 token = 'nAH2VzKrMrRjySLlt8HCdFU3tM2TUuUZgh39NX'
-new_token_1 = 'mBG2VzKrMrRjySLlt8HCdFU3tM2TUuUZgh71MQ'
+new_token_1 = 'mBG2VzKrMrRjySLlt8HCdFU3tM2TUuUZgh71MF'
 new_token_2 = 'nAH2VzKrMrRjySLlt8HCdFU3tM2TUuUZgh39OD'
 new_token_3 = 'bQD2VzKrMqQjySLlt7HCdFU3tM2TUuUZgh84DD'
 new_token_4 = 'bBQ2VzKrMrRjySLlt8HCdFZ3tM2TUuUZgh39MB'
